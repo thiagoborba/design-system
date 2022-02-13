3bf8d8b77841bb2d592687aa86ec057187c800d3
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-02-13 00:46:09 -0300

    feat: adiciona arquivos mdx para documentar componentes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,18 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
   ],
   "framework": "@storybook/react"
 }
